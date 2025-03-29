9ca3018566ae2fdbd5152832c6b8942257077c47
scottc11 <scott.m.campbell11@gmail.com>
2025-03-28 21:48:07 -0400

    changed publicPath to bundle.js in production

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
-    publicPath: '/',
+    publicPath: process.env.NODE_ENV === 'production' ? '/ok-manuals/' : '/',
   },
   devtool: "inline-source-map",
   devServer: {
