982aee42de727c1a01437651aca7e1c3c7af7cb1
scottc11 <scott.m.campbell11@gmail.com>
2025-11-10 20:23:08 -0500

    about styling

--- src/client/views/About.tsx
+++ src/client/views/About.tsx
@@ -6,10 +6,10 @@ import NewsletterSignup from "../components/NewsletterSignup/NewsletterSignup";
 const About = () => {
   return (
     <div className="bg-white text-onyx">
-        <div className="container mx-auto py-8 max-w-3xl">
+        <div className="container mx-auto py-8 max-w-2xl">
             <h1 className="text-4xl font-bungee mb-8 text-left">About</h1>
             <p className="text-onyx text-opacity-75">
-                <b>OK200</b> is a self-run Canadian boutique electronic instrument company founded by me, <b>Scott Campbell</b>. I mainly focus my designs towards being "hands-on" and performative to enhance the playability of a eurorack system during live performance. I use a variety of custom-manufactured components to provide more intuitive and expressive control within a modular synthesizer system.
+                <b>OK200</b> is a self-run Canadian boutique electronic instrument company founded by myself, <b>Scott Campbell</b>. I mainly focus my designs towards being "hands-on" and performative to enhance the playability of a eurorack system during live performance. I use a variety of custom-manufactured components to provide more intuitive and expressive control within a modular synthesizer system.
             </p>
 
             <Divider />
