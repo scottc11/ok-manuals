6e90c5d4f1bcb6c0c009c85f6cef4000c5c355c1
scottc11 <scott.m.campbell11@gmail.com>
2025-11-12 08:55:01 -0500

    updated note background to tiny dots

--- src/client/components/Note/Note.tsx
+++ src/client/components/Note/Note.tsx
@@ -7,7 +7,7 @@ type NoteProps = {
 
 const Note = ({ children }: NoteProps) => {
     return (
-        <div className="px-8 my-4 font-thin italic text-white bg-onyx/50 bg-striped-diagonal-md rounded-sm shadow-onyx/25 shadow-md">
+        <div className="px-8 my-4 font-thin italic text-white bg-onyx/50 [background-image:radial-gradient(rgba(255,255,255,0.08)_1px,transparent_1px)] [background-size:10px_10px] [background-position:0_0] rounded-sm shadow-onyx/25 shadow-md">
             {children}
         </div>
     )
